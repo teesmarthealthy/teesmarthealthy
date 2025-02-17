614f0b6a36c7fd30f3b58c6271a87741487b5bef
teesmarthealthy <teesmarthealthy@gmail.com>
2025-02-17 16:07:41 +0700

    fix last

---
+++ teesmarthealthy/src/assets/js/api.js
@@ -0,0 +1,33 @@
+const PEXELS_API_KEY = 'Ducjg6pUZm37ckuhio7CoWJ3FkqsGaWRu8Vf79RYoYqYf4Tn903qxaneY'; // ใส่ API key ของคุณที่นี่
+
+async function fetchImages(query, per_page = 9) {
+    try {
+        const response = await fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=${per_page}`, {
+            headers: {
+                Authorization: PEXELS_API_KEY
+            }
+        });
+        const data = await response.json();
+        return data.photos;
+    } catch (error) {
+        console.error('Error fetching images:', error);
+        return [];
+    }
+}
+
+async function fetchVideos(query, per_page = 6) {
+    try {
+        const response = await fetch(`https://api.pexels.com/videos/search?query=${query}&per_page=${per_page}`, {
+            headers: {
+                Authorization: PEXELS_API_KEY
+            }
+        });
+        const data = await response.json();
+        return data.videos;
+    } catch (error) {
+        console.error('Error fetching videos:', error);
+        return [];
+    }
+}
+
+export { fetchImages, fetchVideos };
